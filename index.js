9713869d2372bba255415e3857c98f3c9ff7a7f7
Alex Wellnitz <moin@wellnitz-alex.de>
2022-07-20 13:10:18 +0200

    Get the current configured domains.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const config = require('config');
+
+async function startSync() {
+    let domains = await getCurrentConfiguredDomains();
+}
+
+async function getCurrentConfiguredDomains() {
+    let letsEncryptPath = config.letsencrypt.path;
+    let letsEncryptConfigPath = letsEncryptPath + '/config/';
+    let letsEncryptDomainPath = letsEncryptPath + '/live/';
+
+    let domains = [];
+    let configFiles = await fs.readdir(letsEncryptConfigPath);
+
+    console.log(configFiles);
+}
+
+startSync();
